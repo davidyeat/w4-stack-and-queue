83f2faf2247c77b9a376ec499d7d31aa813352f1
chhi-hiro <chhunhourtiek255@gmail.com>
2025-11-11 22:51:13 +0700

    update keywords

--- QUEUE/Queue.hpp
+++ QUEUE/Queue.hpp
@@ -9,38 +9,39 @@ struct CoffeeOrder{
     string drinkType="";
 
 };
-class Queue
-{
-private:
-    CoffeeOrder iteam[Max_Size];
-    int front;
-    int rear;
-public:
-    Queue():front(0), rear(0){}
-    bool isEmpty() const {
-        return front ==rear;}
-    bool isFull()const { 
-        return (rear == Max_Size) &&(front == 0 );}
-    int size() const {
-        return rear - front;}
+class Queue{
+    private:
+        CoffeeOrder QueueList[Max_Size];
+        int front;
+        int rear;
+    public:
+        Queue():front(0), rear(0){}
+        bool isEmpty() const {
+            return front == rear;}
+        bool isFull()const { 
+            return (rear == Max_Size) &&(front == 0 );
+        }
+        int size() const {
+            return rear - front;
+        }
 
-    int capacity() const {
-        return Max_Size;
-    }
+        int capacity() const {
+            return Max_Size;
+        }
 
-    bool enqueue(){
+        bool enqueue(){
 
-    }
+        }
 
-    CoffeeOrder dequeue(){
+        CoffeeOrder dequeue(){
 
-    };
-    CoffeeOrder peek(){
+        };
+        CoffeeOrder peek(){
 
-    };
+        };
 
-    void display(){
+        void display(){
 
-    };
+        };
 };
 
