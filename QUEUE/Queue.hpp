e86f4099fa8a60f02e5ab964ee19d5f8f589022a
OuTharatheb <tharatheb.ou@gmail.com>
2025-11-11 22:20:39 +0700

    queue

--- QUEUE/Queue.hpp
+++ QUEUE/Queue.hpp
@@ -12,17 +12,35 @@ struct CoffeeOrder{
 class Queue
 {
 private:
-    /* data */
+    CoffeeOrder iteam[Max_Size];
+    int front;
+    int rear;
 public:
-    Queue(/* args */);
-    ~Queue();
+    Queue():front(0), rear(0){}
+    bool isEmpty() const {
+        return front ==rear;}
+    bool isFull()const { 
+        return (rear == Max_Size) &&(front == 0 );}
+    int size() const {
+        return rear - front;}
+
+    int capacity() const {
+        return Max_Size;
+    }
+
+    bool enqueue(){
+
+    }
+
+    CoffeeOrder dequeue(){
+
+    };
+    CoffeeOrder peek(){
+
+    };
+
+    void display(){
+
+    };
 };
-
-Queue::Queue(/* args */)
-{
-}
-
-Queue::~Queue()
-{
-}
 
