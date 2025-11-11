841468959225e6dc71217380ed4b4fe8c0c2ed76
OuTharatheb <tharatheb.ou@gmail.com>
2025-11-11 22:00:42 +0700

    Queue

---
+++ QUEUE/Queue.hpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+#define Max_Size 8
+
+struct CoffeeOrder{
+    int num_order;
+    string drinkType="";
+
+};
+class Queue
+{
+private:
+    /* data */
+public:
+    Queue(/* args */);
+    ~Queue();
+};
+
+Queue::Queue(/* args */)
+{
+}
+
+Queue::~Queue()
+{
+}
+
